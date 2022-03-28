f1456beee9592d7892292771191cc2c73eabae5f
Leiver Campeon <leiverandres04p@gmail.com>
2022-03-28 16:41:50 +0000

    Log losses every 10 steps

--- engine.py
+++ engine.py
@@ -31,9 +31,10 @@ def train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq, tb
 
         loss_dict = model(images, targets)
         losses = sum(loss for loss in loss_dict.values())
-        for key, value in loss_dict.items():
-            tb_writer.add_scalar(f"Losses/{key}", value, global_step)    
-        tb_writer.add_scalar("Losses/total_loss", losses, global_step)
+        if global_step % 10 == 0:
+            for key, value in loss_dict.items():
+                tb_writer.add_scalar(f"Losses/{key}", value, global_step)    
+            tb_writer.add_scalar("Losses/total_loss", losses, global_step)
         # reduce losses over all GPUs for logging purposes
         loss_dict_reduced = utils.reduce_dict(loss_dict)
         losses_reduced = sum(loss for loss in loss_dict_reduced.values())
